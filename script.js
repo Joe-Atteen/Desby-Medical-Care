adf817c95fd0719ea81254ba90338d745dabcf63
Joe-Atteen <joeyatteen@gmail.com>
2022-05-30 20:58:25 +0000

    nav link highlight on scroll

--- script.js
+++ script.js
@@ -1,13 +1,36 @@
 //background color on navbar on scroll
 const nav = document.querySelector('nav');
 window.addEventListener('scroll', () => {
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     if (window.scrollY >= 50) {
         nav.classList.add('navback');
     }
     else {
         nav.classList.remove('navback');
     }
+})
+
+//nav link highlight on scroll
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav ul li');
+
+window.addEventListener('scroll', () => {
+    let current = '';
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (scrollY >= (sectionTop - sectionHeight / 3)) {
+            current = section.getAttribute('id');
+        }
+    })
+
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if (li.classList.contains(current)) {
+            li.classList.add('active')
+        }
+    })
+
 })
 
 
